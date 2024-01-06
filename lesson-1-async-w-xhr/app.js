abdc977877213f53fa61c5d6dd4712f8ff8d0a11
182Marco <marcomilza@gmail.com>
2024-01-06 22:28:15 +0100

    singol img displayed

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,12 +1,44 @@
 (function () {
-    const form = document.querySelector('#search-form');
-    const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
-    const responseContainer = document.querySelector('#response-container');
+  const form = document.querySelector("#search-form");
+  const searchField = document.querySelector("#search-keyword");
+  let searchedForText;
+  const responseContainer = document.querySelector("#response-container");
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
-    });
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    responseContainer.innerHTML = "";
+    searchedForText = searchField.value || "hippos";
+
+    const unsplashRequest = new XMLHttpRequest();
+
+    unsplashRequest.open(
+      "GET",
+      `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`
+    );
+    unsplashRequest.setRequestHeader(
+      "Authorization",
+      "Client-ID XgGCWwKPt1zo9_jVDoruVQnmLocqhHn3IzlaOHPTP50"
+    );
+    unsplashRequest.onload = () => addImg();
+
+    unsplashRequest.send();
+
+    function addImg() {
+      const data = JSON.parse(unsplashRequest.responseText);
+      console.log(`marcom ---> data: `, data);
+      const firstImage = data.results[0];
+      let htmlcontent = "";
+
+      if (data?.results) {
+        htmlcontent = `<figure>
+      <img src="${firstImage.urls.regular}" alt="${searchedForText}" />
+      <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+      </figure>`;
+      } else {
+        htmlcontent = `<div class="error-no-image">No images available</div>`;
+      }
+
+      responseContainer.insertAdjacentHTML("afterbegin", htmlcontent);
+    }
+  });
 })();
