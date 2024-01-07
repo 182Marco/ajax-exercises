5d37847888412c00d3e4138335e11166bf7cfb75
182Marco <marcomilza@gmail.com>
2024-01-07 21:20:34 +0100

    with jquery example

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -4,68 +4,65 @@ const getUnsplashQueryUrl = (query) =>
 const getNyTimesQueryUrl = (query) =>
   `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=KAywMcFRlSlaqsToYRVMzQIn9AMIAfhp`;
 
+const addInDoc = (when, html) =>
+  responseContainer.insertAdjacentHTML(where, htm);
+
+const addImg = (res) => {
+  let htmlcontent = "";
+
+  if (res) {
+    htmlcontent = res.map(
+      (e) => `<figure>
+      <img src="${e.urls.regular}" alt="${searched}" />
+      <figcaption>${searched} by ${e.user.name}</figcaption>
+      </figure>`
+    );
+  } else {
+    htmlcontent = `<div class="error-no-image">No images available</div>`;
+  }
+
+  addInDoc("afterbegin", htmlcontent);
+};
+
+const addArticles = (res) => {
+  let htmlcontent = "";
+
+  if (res || res.length) {
+    htmlcontent =
+      "<ul>" +
+      res
+        .map(
+          (e) =>
+            `<li class="article"><h2><a href="${e.web_url}">${e.headline.main}</a></h2><p>${e.snippet}</p></li>`
+        )
+        .join(" ") +
+      "</ul>";
+  } else {
+    htmlcontent = `<div class="error-no-image">No articles available</div>`;
+  }
+  addInDoc("beforeend", htmlcontent);
+};
+
 (function () {
   const form = document.querySelector("#search-form");
   const searchField = document.querySelector("#search-keyword");
-  let searched;
   const responseContainer = document.querySelector("#response-container");
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
-    responseContainer.innerHTML = "";
-    searched = searchField.value;
+    const searched = searchField.value;
 
     const unsplashRequest = new XMLHttpRequest();
-    const articleRequest = new XMLHttpRequest();
 
     unsplashRequest.open("GET", getUnsplashQueryUrl(searched));
 
-    articleRequest.open("GET", getNyTimesQueryUrl(searched));
-    unsplashRequest.setRequestHeader(
-      "Authorization",
-      "Client-ID XgGCWwKPt1zo9_jVDoruVQnmLocqhHn3IzlaOHPTP50"
-    );
-    const getRes = (req) => JSON.parse(req.responseText);
-    unsplashRequest.onload = () => addImg(getRes(unsplashRequest));
-    articleRequest.onload = () => addArticles(getRes(articleRequest));
+    unsplashRequest.onload = () => addImg(JSON.parse(req.responseText));
 
     unsplashRequest.send();
-    articleRequest.send();
 
-    function addImg(res) {
-      let htmlcontent = "";
-
-      if (res) {
-        htmlcontent = res.map(
-          (e) => `<figure>
-        <img src="${e.urls.regular}" alt="${searched}" />
-        <figcaption>${searched} by ${e.user.name}</figcaption>
-        </figure>`
-        );
-      } else {
-        htmlcontent = `<div class="error-no-image">No images available</div>`;
-      }
-
-      responseContainer.insertAdjacentHTML("afterbegin", htmlcontent);
-    }
-
-    function addArticles(res) {
-      let htmlcontent = "";
-
-      if (res || res.length) {
-        htmlcontent =
-          "<ul>" +
-          res
-            .map(
-              (e) =>
-                `<li class="article"><h2><a href="${e.web_url}">${e.headline.main}</a></h2><p>${e.snippet}</p></li>`
-            )
-            .join(" ") +
-          "</ul>";
-      } else {
-        htmlcontent = `<div class="error-no-image">No articles available</div>`;
-      }
-      responseContainer.insertAdjacentHTML("beforeend", htmlcontent);
-    }
+    // example with jquery
+    $.ajax({
+      url: getNyTimesQueryUrl(searched),
+    }).done(() => addArticles(res));
   });
 })();
