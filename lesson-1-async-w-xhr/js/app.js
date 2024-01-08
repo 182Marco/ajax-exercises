d3624eef556271a4a1d2344757260546acb65dc1
182Marco <marcomilza@gmail.com>
2024-01-08 21:41:50 +0100

    feat: done with fetch

--- lesson-1-async-w-xhr/js/app.js
+++ lesson-1-async-w-xhr/js/app.js
@@ -2,17 +2,17 @@ const form = document.querySelector("#search-form");
 const searchField = document.querySelector("#search-keyword");
 const responseContainer = document.querySelector("#response-container");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", e => {
   e.preventDefault();
   const searched = searchField.value;
 
-  const unsplashRequest = new XMLHttpRequest();
-  unsplashRequest.open("GET", getUnsplashQueryUrl(searched));
-  unsplashRequest.onload = () => addImg(JSON.parse(req.responseText));
-  unsplashRequest.send();
+  fetch(getUnsplashQueryUrl(searched))
+    .then(res => res.json())
+    .then(addImg(res))
+    .catch(er => console.error(er));
 
-  // example with jquery
-  $.ajax({
-    url: getNyTimesQueryUrl(searched),
-  }).done(() => addArticles(res));
+  fetch(getNyTimesQueryUrl(searched))
+    .then(res => res.json())
+    .then(addArticles(res))
+    .catch(er => console.error(er));
 });
